d3f0b098fc8263334d5177ffeaff6ea9702d0940
HDJ <263463764@qq.com>
2020-11-12 15:41:38 +0800

    backup 页面 新增导出数据表功能

--- web/devspot/backup.js
+++ web/devspot/backup.js
@@ -1,4 +1,18 @@
 import React from 'react';
 
 import Backup from '@/pages/devops/config/Backup';
-export default (props) => <Backup />;
+
+import DiffCompose from './config/Backup/diffCompose';
+import DownloadTable from './config/Backup/downloadTable';
+
+export default (props) => (
+    <>
+        <div style={{marginBottom:'16px'}}>
+            <DiffCompose/>
+        </div>
+        <div style={{marginBottom:'16px'}}>
+            <DownloadTable/>
+        </div>
+        <Backup />
+    </>
+);
